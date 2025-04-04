35111fdb26afa3a9278a35c94e59ce65cc4161d8
ModzabazeR <moddaeng.pps@gmail.com>
2025-04-05 00:26:40 +0700

    update user entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -12,15 +12,6 @@ export class Users extends Model {
   })
   UserID: string;
 
-  @Column({ type: DataType.STRING })
-  FName: string;
-
-  @Column({ type: DataType.STRING })
-  LName: string;
-
-  @Column({ type: DataType.DATE })
-  BirthDate: Date;
-
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   Email: string;
 
@@ -30,12 +21,54 @@ export class Users extends Model {
   @Column({ type: DataType.DATE, defaultValue: DataType.NOW })
   UpdatedDT: Date;
 
-  @Column({ type: DataType.ENUM('male', 'female', 'other') })
-  Sex: 'male' | 'female' | 'other';
-
   @Column({
     type: DataType.ENUM('admin', 'user', 'project-approver'),
     defaultValue: 'user',
   })
   Role: 'admin' | 'user' | 'project-approver';
+
+  // ข้อมูลที่ต้องกรอกตอนส่งโปรเจกต์
+  @Column({ type: DataType.ENUM('male', 'female', 'other') })
+  Sex: 'male' | 'female' | 'other';
+
+  @Column({ type: DataType.STRING })
+  FName: string;
+
+  @Column({ type: DataType.STRING })
+  LName: string;
+
+  @Column({ type: DataType.DATE })
+  BirthDate: Date;
+
+  // เด็กชาย เด็กหญิง นาย นาง นางสาว
+  @Column({
+    type: DataType.ENUM('Master', 'Miss', 'Mr', 'Mrs', 'Ms'),
+  })
+  Prefix: 'Master' | 'Miss' | 'Mr' | 'Mrs' | 'Ms';
+
+  // ประถมศึกษา มัธยมศึกษา ปริญญาตรี ปริญญาโท ปริญญาเอก ปวช ปวส
+  @Column({
+    type: DataType.ENUM(
+      'elementary',
+      'secondary',
+      'bachelor',
+      'master',
+      'doctoral',
+      'vocational_certificate',
+      'high_vocational_certificate',
+    ),
+  })
+  Education:
+    | 'elementary'
+    | 'secondary'
+    | 'bachelor'
+    | 'master'
+    | 'doctoral'
+    | 'vocational_certificate'
+    | 'high_vocational_certificate';
+
+  @Column({
+    type: DataType.STRING,
+  })
+  Tel: string;
 }
