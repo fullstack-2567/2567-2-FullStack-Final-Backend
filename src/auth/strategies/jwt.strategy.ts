44368de600e2bebf128d0a516907da3fed035628
ModzabazeR <moddaeng.pps@gmail.com>
2025-04-08 09:50:02 +0700

    alpha-4

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,6 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../../users/users.service';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
@@ -13,7 +14,8 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (req) => {
+        (req: Request) => {
+          console.log(req.cookies);
           const accessToken = req.cookies['access_token'];
           if (!accessToken) return null;
           return accessToken;
--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ import { apiReference } from '@scalar/nestjs-api-reference';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { ConfigService } from '@nestjs/config';
 import { urlencoded, json } from 'express';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(cookieParser());
   app.enableCors({
     origin: ['http://localhost:5173'],
     credentials: true,
