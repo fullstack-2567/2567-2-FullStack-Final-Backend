18b248dacef1c22ff23ca6d1b91d05a7f7de22c7
ModzabazeR <moddaeng.pps@gmail.com>
2025-04-13 15:17:00 +0700

    update api docs

--- src/dto/response/dashboard/projects.response.dto.ts
+++ src/dto/response/dashboard/projects.response.dto.ts
@@ -8,15 +8,7 @@ export class ProjectCountDto {
   thisMonth: number;
 }
 
-export class ProjectTypeCountDto {
-  @ApiProperty({ description: 'Type of project' })
-  type: string;
-
-  @ApiProperty({ description: 'Number of projects of this type' })
-  count: number;
-}
-
-export class ProjectsDashboardDataDto {
+export class ProjectSummaryDto {
   @ApiProperty({ type: () => ProjectCountDto })
   pending_projects: ProjectCountDto;
 
@@ -25,9 +17,64 @@ export class ProjectsDashboardDataDto {
 
   @ApiProperty({ type: () => ProjectCountDto })
   rejected_projects: ProjectCountDto;
+}
 
-  @ApiProperty({ type: [ProjectTypeCountDto] })
-  project_types: ProjectTypeCountDto[];
+export class ProjectTypeDto {
+  @ApiProperty({ description: 'Type of project' })
+  type: string;
+
+  @ApiProperty({ description: 'Number of projects of this type' })
+  count: number;
+}
+
+export class ProjectStatusDto {
+  @ApiProperty({ description: 'Number of projects pending first approval' })
+  pending_first_approval: number;
+
+  @ApiProperty({ description: 'Number of projects with first approval' })
+  first_approved: number;
+
+  @ApiProperty({ description: 'Number of projects with second approval' })
+  second_approved: number;
+
+  @ApiProperty({ description: 'Number of projects with third approval' })
+  third_approved: number;
+
+  @ApiProperty({ description: 'Number of rejected projects' })
+  rejected: number;
+}
+
+export class SdgTypeDto {
+  @ApiProperty({ description: 'SDG type' })
+  sdg: string;
+
+  @ApiProperty({ description: 'Number of projects with this SDG type' })
+  count: number;
+}
+
+export class ChildProjectsDto {
+  @ApiProperty({ description: 'Number of normal projects' })
+  normal: number;
+
+  @ApiProperty({ description: 'Number of child projects' })
+  child: number;
+}
+
+export class ProjectsDashboardDataDto {
+  @ApiProperty({ type: () => ProjectSummaryDto })
+  summary: ProjectSummaryDto;
+
+  @ApiProperty({ type: [ProjectTypeDto] })
+  popular_project_types: ProjectTypeDto[];
+
+  @ApiProperty({ type: () => ProjectStatusDto })
+  project_status: ProjectStatusDto;
+
+  @ApiProperty({ type: [SdgTypeDto] })
+  popular_sdg_types: SdgTypeDto[];
+
+  @ApiProperty({ type: () => ChildProjectsDto })
+  child_projects: ChildProjectsDto;
 }
 
 export class ProjectsDashboardResponseDto {
